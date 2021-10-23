6b9d45bb4ae39ce74f0cdab3be874d9d0563c373
Apolbaq <emilianopolanco5@gmail.com>
2021-10-23 16:09:15 +0200

    Bingo complete

--- Bingo/bingo.js
+++ Bingo/bingo.js
@@ -1,23 +1,27 @@
-//Creation of the Bingo card and the Pool of numbers to play bingo
+//Key variables for the game
 let turnCounter = 0;
 let foundLine = false;
 let win = false;
-let cardSize = 0;
-let bingoBall;
+let cardSize = 4;
+let bingoBall;          //This is the "ball" that contains the number
+let bingoCard = {};
+let userCardAnswer = 'yes';
 let poolNumbers = [];
+let players = [
+    {Name: 'Giorno', Score: 900},
+    {Name: 'GianMarco', Score: 430},
+    {Name: 'Papapu', Score: 420},
+    {Name: 'Stevan', Score: 324}];
 let resetPoolNumber = ()=>{
     let tempPool = [];
     for(let i=0; i<90; i++){
         tempPool[i]=i+1;
     }
     poolNumbers = tempPool;
-}
-resetPoolNumber();
-
-let bingoCard = {
-	numBoard: createBoard(),
-}
-function createBoard(){
+}//end function
+resetPoolNumber();          //I start creating pool of Numbers
+//A lot of functions....
+function createBingoCardBoard(){
     let tempBoard = [];
     for(let j = 0; j<cardSize; j++){
         tempBoard[j]= [];
@@ -26,30 +30,30 @@ function createBoard(){
             tempBoard[j][i]= poolNumbers[indexHolder];
             poolNumbers.splice(indexHolder, 1);
         }//end for1
-    }//end for2
-    return tempBoard;
+    }//end for2                                                 //I need to reset the Pool of Numbers
+    bingoCard.board = tempBoard;                                //because when i create a new bingo card
+    resetPoolNumber();                                          //I take the elements from there
 }//end function
-function randomIndexPoolNumbers(){                                    //This number its the index of the poolNumbers
+function randomIndexPoolNumbers(){                              //This number its the index of the poolNumbers
 	return parseInt(Math.random()*poolNumbers.length);
-}
-resetPoolNumber();
-//Now its time to declare the functions that fill that bingo Card :)
+}//end function
 
 let generateBall = () =>{
-    let indexHolder = randomIndexPoolNumbers();
-    bingoBall = poolNumbers[indexHolder];
-    poolNumbers.splice(indexHolder, 1);
+    let indexHolder = randomIndexPoolNumbers();                 //This variable hold the random Index to
+    bingoBall = poolNumbers[indexHolder];                       //Find the number in the pool of Numbers and to
+    poolNumbers.splice(indexHolder, 1);               //Delete the number from the pool of Numbers
     console.log(`Number ${bingoBall} someone has the number ${bingoBall}?`)
-}
+}//end function
 
- let checkBallinBingoCard = () =>{      //checks the ball in the board and places and x if founded
+ let checkBallinBingoCard = () =>{                               //Checks the ball in the board
      for(let i = 0; i< cardSize; i++){
          for(let j = 0; j< cardSize; j++){
-             if(bingoCard.numBoard[i][j] ===bingoBall){
-                bingoCard.numBoard[i][j] = 'x';
+             if(bingoCard.board[i][j] ===bingoBall){
+                bingoCard.board[i][j] = 'x';                    //And places and x if founded
              }
          }
      }
+     console.table(bingoCard.board);                            //Feedback to the user
  }//end function
 
  let checkLineinBingoCard = () =>{
@@ -58,35 +62,68 @@ let generateBall = () =>{
         let isRowFull = true;                   //checks for a vertical line
         let isColumnFull = true;                //checks for an horizontal line
         for(let j = 0; j< cardSize; j++){
-            isRowFull = isRowFull && (bingoCard.numBoard[i][j] ==='x');
-            isColumnFull = isColumnFull && (bingoCard.numBoard[j][i] ==='x');
-            isBingo = isBingo && (bingoCard.numBoard[i][j] ==='x');
+            isRowFull = isRowFull && (bingoCard.board[i][j] ==='x');
+            isColumnFull = isColumnFull && (bingoCard.board[j][i] ==='x');
+            isBingo = isBingo && (bingoCard.board[i][j] ==='x');
         }
         if(isRowFull&&!foundLine){
             foundLine = true;
-            console.log("Horizontal Line");
+            console.log('Ⓗⓞⓡⓘⓩⓞⓝⓣⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓗⓞⓡⓘⓩⓞⓝⓣⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓗⓞⓡⓘⓩⓞⓝⓣⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓗⓞⓡⓘⓩⓞⓝⓣⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓗⓞⓡⓘⓩⓞⓝⓣⓐⓛ ⓛⓘⓝⓔ');
         }
         if(isColumnFull&&!foundLine){
             foundLine = true;
-            console.log("Vertical Line");
+            console.log('Ⓥⓔⓡⓣⓘⓒⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓥⓔⓡⓣⓘⓒⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓥⓔⓡⓣⓘⓒⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓥⓔⓡⓣⓘⓒⓐⓛ ⓛⓘⓝⓔ');
+            console.log('Ⓥⓔⓡⓣⓘⓒⓐⓛ ⓛⓘⓝⓔ');
         }
     }
     if(isBingo){
         win = true;
-        console.log('Bingooooooooo');
+        scoreGenerator();
+        console.log('🅱🅸🅽🅶🅾');
+        console.log('🅱🅸🅽🅶🅾');
+        console.log('🅱🅸🅽🅶🅾');
+        console.log('🅱🅸🅽🅶🅾');
+        console.log('🅱🅸🅽🅶🅾');
     }
  }//end function
-
+let scoreGenerator = () =>{
+    let Score = parseInt(1000/turnCounter);
+    players.push({Name, Score});
+    console.table(players);
+}//end function
+let offerCard = ()=>{                       //This is a recursive function that keeps offering a card until you are happy
+    cardSize = window.prompt('How Big you want your card (0-9)?', 4);
+    createBingoCardBoard();
+    console.table(bingoCard.board);         //Shows to the user the new created Bingo card
+    userCardAnswer = window.prompt('Are you happy with this card?', 'yes');
+    if(userCardAnswer==='yes'){
+        window.alert('LETSS PLAY');
+        while(confirm('Here comes the ball......')&&!win){
+            turnCounter ++;
+            console.log(`Turn: ${turnCounter}`);
+            generateBall();
+            checkBallinBingoCard();
+            checkLineinBingoCard();
+        }
+    }else {
+        offerCard(); //Recursion lol
+    }
+}//end function
 //Main routine for the user
-window.prompt('hi');
-while(confirm('Another number?')){
-    turnCounter ++;
-    console.log(`This is the turn ${turnCounter}`);
-    generateBall();
-    checkBallinBingoCard();
-    console.table(bingoCard.numBoard);
-    checkLineinBingoCard();
-}
+let Name = window.prompt('Wanna play some bingo? Mr. or Ms...','Charlie');
+window.alert('Everyone starts with 1000 point but the more you take to complete your card the less points you have');
+offerCard();
+console.log('Thanks for playing');
 
-//CHANGE THE VARIABLE NUMBOARD TO JUST BOARD IN VSCODE
+
+
+
+
  
